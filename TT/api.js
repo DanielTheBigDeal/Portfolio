72f0ff0bef865442e03e5c7bc7bfa3a95fce46da
Daniel Khokhlov <danilhohlov284@gmail.com>
2023-10-13 16:12:16 +0700

    footer, select and calendar are done

--- TT/api.js
+++ TT/api.js
@@ -154,71 +154,413 @@ let result = [
             action: "Купить" 
           }, 
     url: "https://frontendconf.ru/moscow/2024" 
-  }
+  },
 ];
 
 const $eventsList = document.getElementById('eventsList');
-
-/*function detectYear(year) {
-  if (year.value = 2023) {
-    console.log(year.value);
-  }
-}*/
-
-/*function January() {
-
-  for (const item of $eventsList) {
-    if (item.date_range.includes('январ')) {
-      
-    }
-  }
-}*/
-
-
 const year = document.getElementById('footer-year');
 
 const year2023 = document.getElementById('2023');
 const year2024 = document.getElementById('2024');
 
-
-year.onchange = () => {
+// a function for choosing a year
+function changeYear() {
   let yearV = year.value;
-
-  if (yearV = 2024) {
-    year2024.classList.add('d-flex');
+  if (yearV == 2024) {
     year2024.classList.remove('none');
+    year2024.classList.add('d-flex');
     year2023.classList.add('none'); 
+    packMarch2024()
+  }
+  else if (yearV == 2023) {
+    year2023.classList.remove('none');
+    year2023.classList.add('d-flex');
+    year2024.classList.add('none');
+    location.reload();
   }
 }
 
+const $point = document.createElement('span');
+$point.classList.add('pointer');
+const $wrapper = document.querySelector('.footer__list');
+$wrapper.prepend($point);
+
+const $emptyLi = document.createElement('li');
+$emptyLi.textContent = 'В этом месяце нет событий';
+$emptyLi.classList.add('empty');
+
+// functions to show months in 2023
+
 function packJanuary2023(obj) {
+  $eventsList.innerHTML = '';
   let january2023 = [];
   for (obj of result) {
     if (obj.date_range.includes('января 2023')) {
-      january2023.push(obj)
+      january2023.push(obj);
     }
-    // else if (january2023 == null) {
-    //   $eventsList.innerHTML = '<li><h2>В этом месяце нет событий</h2></li>'
-    // } doesn't work
   }
+  if (january2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '7px';
   showCard(january2023);
 }
 
+function packFebruary2023(obj) {
+  $eventsList.innerHTML = '';
+  let february2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('февраля 2023')) {
+      february2023.push(obj);
+    }
+  }
+  if (february2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '124px';
+  showCard(february2023);
+}
+
+function packMarch2023(obj) {
+  $eventsList.innerHTML = '';
+  let march2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('марта 2023')) {
+      march2023.push(obj);
+    }
+  }
+  if (march2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '231px';
+  showCard(march2023);
+}
+
+function packApril2023(obj) {
+  $eventsList.innerHTML = '';
+  let april2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('апреля 2023')) {
+      april2023.push(obj);
+    }
+  }
+  if (april2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '331px';
+  showCard(april2023);
+}
+
+function packMay2023(obj) {
+  $eventsList.innerHTML = '';
+  let may2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('мая 2023')) {
+      may2023.push(obj);
+    }
+  }
+  if (may2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '431px';
+  showCard(may2023);
+}
+
+function packJune2023(obj) {
+  $eventsList.innerHTML = '';
+  let june2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('июня 2023')) {
+      june2023.push(obj);
+    }
+  }
+  if (june2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '523px';
+  showCard(june2023);
+}
+
+function packJuly2023(obj) {
+  $eventsList.innerHTML = '';
+  let july2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('июля 2023')) {
+      july2023.push(obj);
+    }
+  }
+  if (july2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '621px';
+  showCard(july2023);
+}
+
+function packAugust2023(obj) {
+  $eventsList.innerHTML = '';
+  let august2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('августа 2023')) {
+      august2023.push(obj);
+    }
+  }
+  if (august2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '724px';
+  showCard(august2023);
+}
+
+function packSeptember2023(obj) {
+  $eventsList.innerHTML = '';
+  let september2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('сентября 2023')) {
+      september2023.push(obj);
+    }
+  }
+  if (september2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '837px';
+  showCard(september2023);
+}
+
+function packOctober2023(obj) {
+  $eventsList.innerHTML = '';
+  let october2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('октября 2023')) {
+      october2023.push(obj);
+    }
+  }
+  if (october2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '960px';
+  showCard(october2023);
+}
 
 function packNovember2023(obj) {
+  $eventsList.innerHTML = '';
   let november2023 = [];
   for (obj of result) {
     if (obj.date_range.includes('ноября 2023')) {
       november2023.push(obj)
     }
   }
+  if (november2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '1075px';
   showCard(november2023);
+}
+
+function packDecember2023(obj) {
+  $eventsList.innerHTML = '';
+  let december2023 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('декабря 2023')) {
+      december2023.push(obj)
+    }
+  }
+  if (december2023.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point.style.left = '1190px';
+  showCard(december2023);
+}
+
+const $point2 = document.createElement('span');
+$point2.classList.add('pointer');
+const $wrapper2 = document.querySelector('.second');
+$wrapper2.prepend($point2);
+
+// functions to show months in 2023
+
+function packJanuary2024(obj) {
+  $eventsList.innerHTML = '';
+  let january2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Январь 2024')) {
+      january2024.push(obj);
+    }
+  }
+  if (january2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '7px';
+  showCard(january2024);
+}
+
+function packFebruary2024(obj) {
+  $eventsList.innerHTML = '';
+  let february2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Февраль 2024')) {
+      february2024.push(obj);
+    }
+  }
+  if (february2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '124px';
+  showCard(february2024);
+}
+
+function packMarch2024(obj) {
+  $eventsList.innerHTML = '';
+  let march2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Март 2024')) {
+      march2024.push(obj);
+    }
+  }
+  if (march2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '231px';
+  showCard(march2024);
+}
+
+function packApril2024(obj) {
+  $eventsList.innerHTML = '';
+  let april2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Апрель 2024')) {
+      april2024.push(obj);
+    }
+  }
+  if (april2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '331px';
+  showCard(april2024);
+}
+
+function packMay2024(obj) {
+  $eventsList.innerHTML = '';
+  let may2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Май 2024')) {
+      may2024.push(obj);
+    }
+  }
+  if (may2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '431px';
+  showCard(may2024);
+}
+
+function packJune2024(obj) {
+  $eventsList.innerHTML = '';
+  let june2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Июнь 2024')) {
+      june2024.push(obj);
+    }
+  }
+  if (june2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '523px';
+  showCard(june2024);
+}
+
+function packJuly2024(obj) {
+  $eventsList.innerHTML = '';
+  let july2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Июль 2024')) {
+      july2024.push(obj);
+    }
+  }
+  if (july2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '621px';
+  showCard(july2024);
+}
+
+function packAugust2024(obj) {
+  $eventsList.innerHTML = '';
+  let august2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Август 2024')) {
+      august2024.push(obj);
+    }
+  }
+  if (august2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '724px';
+  showCard(august2024);
+}
+
+function packSeptember2024(obj) {
+  $eventsList.innerHTML = '';
+  let september2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Сентябрь 2024')) {
+      september2024.push(obj);
+    }
+  }
+  if (september2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '837px';
+  showCard(september2024);
+}
+
+function packOctober2024(obj) {
+  $eventsList.innerHTML = '';
+  let october2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Октябрь 2024')) {
+      october2024.push(obj);
+    }
+  }
+  if (october2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '960px';
+  showCard(october2024);
+}
+
+function packNovember2024(obj) {
+  $eventsList.innerHTML = '';
+  let november2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Ноябрь 2024')) {
+      november2024.push(obj)
+    }
+  }
+  if (november2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '1075px';
+  showCard(november2024);
+}
+
+function packDecember2024(obj) {
+  $eventsList.innerHTML = '';
+  let december2024 = [];
+  for (obj of result) {
+    if (obj.date_range.includes('Декабрь 2024')) {
+      december2024.push(obj)
+    }
+  }
+  if (december2024.length == 0) {
+    $eventsList.append($emptyLi);
+  }
+  $point2.style.left = '1190px';
+  showCard(december2024);
 }
 
 function showCard(arr) {
   for (const item of arr) {
     $Event = document.createElement('li');
-    $Event.classList.add('col-3')
     $Event.classList.add('events__event');
 
     const $EventDate = document.createElement('h3');
@@ -253,12 +595,11 @@ function showCard(arr) {
     $EventDescr.textContent = item.brief;
     $EventLocation.textContent = item.location;
     $EventLink.href = item.url;
-    $EventLink.textContent = item.url;
+    $EventLink.textContent = item.url.replace('https://', '');
 
     let buy = item.buy;
     const $EventBuy = document.createElement('span');
     $EventBuy.classList.add('events__buy');
-
     const $EventBtn = document.createElement('button');
     $EventBtn.classList.add('events__btn');
     const $EventTicketLink = document.createElement('a');
@@ -279,3 +620,5 @@ function showCard(arr) {
     $eventsList.append($Event);
   }
 }
+
+packOctober2023();
